d59e68774cf07e7c404c19022a4039d381867e86
OKitel <kitelolga@gmail.com>
2024-08-07 12:41:39 +0200

    feat: update Cart API URL (task 9)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://p5o62tfcp1.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://OKitel-cart-api-prod.eu-central-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
